034f102779205704aa84c209d5bca84c48edb29b
healthcarefrontend <himchan9494@gmail.com>
2021-04-28 13:58:40 +0900

    redux, firestore etc

--- image-community/src/elements/Text.js
+++ image-community/src/elements/Text.js
@@ -2,14 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const Text = (props) => {
-  const { bold, color, size, children } = props;
+  const { bold, color, size, children, margin } = props;
 
-  const styles = {bold: bold, color: color, size: size};
-  return (
-      <P {...styles}>
-          {children}
-      </P>
-  )
+  const styles = { bold: bold, color: color, size: size, margin };
+  return <P {...styles}>{children}</P>;
 };
 
 Text.defaultProps = {
@@ -17,12 +13,14 @@ Text.defaultProps = {
   bold: false,
   color: "#222831",
   size: "14px",
+  margin: false,
 };
 
 const P = styled.p`
   color: ${(props) => props.color};
   font-size: ${(props) => props.size};
-  font-weight: ${(props) => (props.bold? "600" : "400")};
+  font-weight: ${(props) => (props.bold ? "600" : "400")};
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
 `;
 
 export default Text;
--- image-community/src/index.js
+++ image-community/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './shared/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./shared/App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./redux/configureStore";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
- </BrowserRouter>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
